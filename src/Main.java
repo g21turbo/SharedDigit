7187b7d2017c951ef88384703a847201c05d1d02
g21turbo <dg21stew@gmail.com>
2023-11-05 11:32:01 -0800

    Changed the method of input validation

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,29 @@ public class Main {
 
     }
 
-    public static boolean hasSharedDigit(int numberOne, int numberTwo) {
+    public static boolean hasSharedDigit(int num1, int num2) {
 
-        if (numberOne < 10 || numberOne > 99 || numberTwo < 10 || numberTwo > 99) {
+        boolean firstValid = num1 > 9 && num1 < 100;
+        boolean secondValid = num2 > 9 && num2 < 100;
+
+        if (!firstValid || !secondValid) {
             return false;
+        }
+
+//    --------previous input validation-----------
+//        if (num1 < 10 || num1 > 99 ||
+//                num2 < 10 || num2 > 99) {
+//            return false;
+//        }
+
+        while (num1 > 10 && num2 > 10) {
+            int remDigit1 = num1 % 10;
+            int remDigit2 = num2 % 10;
+
+            if (remDigit1 == remDigit2) {
+                return true;
+            }
         }
         return true;
     }
-
-
-
 }
